12b5b8592327d66814341f4c92679a127840f301
kilonzi <martinkilonzi748@gmail.com>
2024-10-22 23:13:21 +0300

    sync goddamn it

--- lib/Themes/colors.dart
+++ lib/Themes/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class colors {
-  static const appbar = Color.fromARGB(255, 136, 73, 218);
+  static const appbar = Color.fromARGB(255, 32, 155, 243);
   static const activePage = Color.fromARGB(223, 14, 14, 14);
 }
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -37,91 +37,90 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              title: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.account_circle_outlined,
-                      size: 50,
-                    ),
-                    SizedBox(
-                      width: 12,
-                    ),
-                    Text(
-                      'Good Evening, Lucy',
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                  ]),
-              backgroundColor: colors.appbar,
-              expandedHeight: 450.0,
-              toolbarHeight: 150.0,
-              floating: false,
-              pinned: true,
-              stretch: true,
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                collapseMode: CollapseMode.parallax,
-                background: Container(
-                  decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [
-                        colors.appbar,
-                        Colors.black,
-                        Colors.white,
-                      ])),
-                  child: const ClipRRect(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft:
-                          Radius.circular(30.0), // Adjust the radius as needed
-                      bottomRight: Radius.circular(30.0),
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                title: const Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.account_circle_outlined,
+                        size: 50,
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        'Good Evening, Lucy',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ]),
+                backgroundColor: Colors.yellow.shade400,
+                expandedHeight: 450.0,
+                toolbarHeight: 150.0,
+                floating: false,
+                pinned: true,
+                stretch: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  collapseMode: CollapseMode.parallax,
+                  background: Container(
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                          Colors.black,
+                          colors.appbar,
+                          Colors.white,
+                        ])),
+                    child: const ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(
+                            30.0), // Adjust the radius as needed
+                        bottomRight: Radius.circular(30.0),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.vertical(
-                  bottom: Radius.circular(
-                      30.0), // Border radius for the collapsed state
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(
+                        30.0), // Border radius for the collapsed state
+                  ),
                 ),
-              ),
-            )
-          ];
-        },
-        body: const Center(
-          child: Text('Leaderboard'),
-        ),
-      ),
+              )
+            ];
+          },
+          body: Container(
+            decoration: const BoxDecoration(color: Colors.white),
+          )),
       bottomNavigationBar: BottomNavyBar(
-        borderRadius: BorderRadius.only(
+        borderRadius: const BorderRadius.only(
             topRight: Radius.circular(10), topLeft: Radius.circular(10)),
         backgroundColor: Colors.white,
         selectedIndex: _selectedIndex,
         onItemSelected: _onItemSelected,
         items: [
           BottomNavyBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
+            icon: const Icon(Icons.home),
+            title: const Text('Home'),
             activeColor: colors.activePage,
           ),
           BottomNavyBarItem(
-            icon: Icon(Icons.analytics_rounded),
-            title: Text('Leaderboard'),
+            icon: const Icon(Icons.analytics_rounded),
+            title: const Text('Leaderboard'),
             activeColor: colors.activePage,
           ),
           BottomNavyBarItem(
-            icon: Icon(Icons.donut_large),
-            title: Text('Analytics'),
+            icon: const Icon(Icons.donut_large),
+            title: const Text('Analytics'),
             activeColor: colors.activePage,
           ),
           BottomNavyBarItem(
-            icon: Icon(Icons.settings),
-            title: Text('Settings'),
+            icon: const Icon(Icons.settings),
+            title: const Text('Settings'),
             activeColor: colors.activePage,
           ),
         ],
